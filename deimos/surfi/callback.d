e6a53c98b291d0b63c3c624d0f0b6a0bb2fed498
David <admin@dav1d.de>
2013-04-24 17:01:05 +0200

    initial commit

---
+++ deimos/surfi/callback.d
@@ -0,0 +1,91 @@
+import deimos.surfi.surfi;
+
+extern(C) {
+    struct WebKitDOMHTMLElement {}
+    struct WebKitGeolocationPolicyDecision {}
+    struct WebKitHitTestResult {}
+//     struct WebKitNavigationResponse {}
+    alias int WebKitNavigationResponse;
+    struct WebKitNetworkRequest {}
+    struct WebKitWebFrame {}
+    struct WebKitWebNavigationAction {}
+    struct WebKitWebPolicyDecision {}
+    struct WebKitWebResource {}
+    struct GHashTable {}
+    struct GObject {}
+
+    void surfi_client_set_callback_close_web_view(SurfiClient* client,
+        bool function(SurfiClient* client) callback);
+    void surfi_client_set_callback_console_message(SurfiClient* client,
+        bool function(SurfiClient* client, char* message, int line, char* source_id) callback);
+    void surfi_client_set_callback_context_menu(SurfiClient* client,
+        bool function(SurfiClient* client, GtkWidget* default_menu,
+            WebKitHitTestResult* hit_test_result, bool triggered_with_keyboard) callback);
+    void surfi_client_set_callback_copy_clipboard(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_create_plugin_widget(SurfiClient* client,
+        void function(SurfiClient* client, char* mime_type, char* uri, GHashTable* param) callback);
+    void surfi_client_set_callback_create_webview(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_cut_clipboard(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_database_quota_exceeded(SurfiClient* client,
+        void function(SurfiClient* client, GObject* frame, GObject* database) callback);
+    void surfi_client_set_callback_document_load_finished(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_download_requested(SurfiClient* client,
+        void function(SurfiClient* client,GObject* download) callback);
+    void surfi_client_set_callback_editing_began(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_editing_ended(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_entering_fullscreen(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitDOMHTMLElement* element) callback);
+    void surfi_client_set_callback_frame_created(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_geolocation_policy_decision_cancelled(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_geolocation_policy_decision_requested(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitWebFrame* frame, WebKitGeolocationPolicyDecision* policy_decision) callback);
+    void surfi_client_set_callback_hovering_over_link(SurfiClient* client,
+        void function(SurfiClient* client, char* title, char* uri) callback);
+    void surfi_client_set_callback_icon_loaded(SurfiClient* client,
+        void function(SurfiClient* client, char* icon_uri) callback);
+    void surfi_client_set_callback_leaving_fullscreen(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitDOMHTMLElement* element) callback);
+    void surfi_client_set_callback_load_commited(SurfiClient* client,
+        void (*callback)(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_load_error(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_load_finished(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_load_progress_changed(SurfiClient* client,
+        void function(SurfiClient* client, int progress) callback);
+    void surfi_client_set_callback_load_started(SurfiClient* client,
+        void (*callback)(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_mime_type_policy_decision_requested(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitWebFrame* frame, WebKitNetworkRequest* request,
+                        char* mimetype, WebKitWebPolicyDecision* policy_decision) callback);
+    void surfi_client_set_callback_move_cursor(SurfiClient* client,
+        bool function(SurfiClient* client, GtkMovementStep step, int count) callback);
+    void surfi_client_set_callback_navigation_policy_decision_requested(SurfiClient* client,
+        bool function(SurfiClient* client,  WebKitWebFrame* frame, WebKitNetworkRequest* request,
+                        WebKitWebNavigationAction* navigation_action, WebKitWebPolicyDecision* policy_decision) callback);
+    void surfi_client_set_callback_navigation_requested(SurfiClient* client,
+        WebKitNavigationResponse function(SurfiClient* client, WebKitWebFrame* frame, WebKitNetworkRequest* request) callback);
+    void surfi_client_set_callback_new_window_policy_decision_requested(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitWebFrame* frame, WebKitNetworkRequest* request,
+                        WebKitWebNavigationAction* navigation_action, WebKitWebPolicyDecision* policy_decision) callback);
+    void surfi_client_set_callback_onload_event(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_paste_clipboard(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_populate_popup(SurfiClient* client,
+        void function(SurfiClient* client, GtkMenu* menu) callback);
+    void surfi_client_set_callback_print_requested(SurfiClient* client,
+        bool function(SurfiClient* client, WebKitWebFrame* frame) callback);
+    void surfi_client_set_callback_redo(SurfiClient* client,
+        void function(SurfiClient* client) callback);
+    void surfi_client_set_callback_resource_content_length_received(SurfiClient* client,
+        void function(SurfiClient* client, WebKitWebFrame* frame, WebKitWebResource* resource, int length) callback);
+}
---
+++ deimos/surfi/pixbuf.d
@@ -0,0 +1,15 @@
+extern(C) {
+    struct Pixbuf {}
+
+    int surfi_pixbuf_get_n_channels(Pixbuf *pixbuf);
+    bool surfi_pixbuf_get_has_alpha(Pixbuf *pixbuf);
+    int surfi_pixbuf_get_bits_per_sample(Pixbuf *pixbuf);
+    ubyte* surfi_pixbuf_get_pixels(Pixbuf *pixbuf);
+    ubyte* surfi_pixbuf_get_pixels_with_length(Pixbuf *pixbuf, uint *length);
+    int surfi_pixbuf_get_width(Pixbuf *pixbuf);
+    int surfi_pixbuf_get_height(Pixbuf *pixbuf);
+    int surfi_pixbuf_get_rowstride(Pixbuf *pixbuf);
+    uint surfi_pixbuf_get_byte_length(Pixbuf *pixbuf);
+    const(char)* surfi_pixbuf_get_option(Pixbuf *pixbuf, const(char)* key);
+    bool surfi_pixbuf_save(Pixbuf *pixbuf, const(char)* filename, const(char)* type);
+}
